ca3c77513cfd62ec7d7eab42d08c848a0c6fd62a
Isabelle Nauber-Gelhaar <isabelle.nauber-gelhaar@dci-student.org>
2025-07-16 12:06:24 +0200

    1

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
-const API_KEY = 'c9bed387'; // <-- Hier deinen API-Key einfügen
+const API_KEY = 'c9bed387';
 
-// Beispiel-IDs für jede Reihe
+
 const reihe1 = [
     "tt13443470", // The Black Phone 2
-    "tt29644189",// Wednesday
-    "tt0083658", // Blade Runner
-    "tt33043892" // Dexter: Resurrection
+    "tt29644189", // Wednesday
+    "tt0083658",  // Blade Runner
+    "tt33043892", // Dexter: Resurrection
+    "tt0110912", //  Pulp Fiction
+    "tt0111161", //  The Shawshank Redemption
+    "tt0468569", //  The Dark Knight
+    "tt0133093", //  The Matrix
 ];  
 
 const reihe2 = [
     "tt0120903",  // X-Men
-    "tt1490017",   // The Lego Movie
+    "tt1490017",  // The Lego Movie
     "tt0175142",  // Scary Movie
-    "tt0159206"   // Sex and the City
+    "tt0159206",  // Sex and the City
+    "tt0120737",  // The Lord of the Rings: The Fellowship of the Ring
+    "tt0109830",  // Forrest Gump
+    "tt0114369",  // Se7en
+    "tt0102926",  // The Silence of the Lambs
 ];
 
 const reihe3 = [
@@ -20,6 +28,10 @@ const reihe3 = [
     "tt0078346",  // Superman
     "tt0227538",  // Spy Kids
     "tt0462538",  // The Simpsons Movie
+    "tt0443706", //  Zodiac
+    "tt2267998", //  Gone Girl
+    "tt0353969", //  Memories of Murder
+    "tt1392214", // Prisoners
 ];
 
 // Funktion um Daten von der OMDb API zu holen
@@ -32,9 +44,11 @@ async function ladeFilme(reihe, elementId) {
         const div = document.createElement("div");
         div.className = "movie";
         div.innerHTML = `
-            <img src="${film.Poster}" alt="${film.Title}">
-            <h3>${film.Title}</h3>
-            <p>${film.Year}</p>
+        <img src="${film.Poster}" alt="${film.Title}">
+        <div class="info">
+        <h3>${film.Title}</h3>
+        <p>${film.Year}</p>
+        </div>
         `;
         container.appendChild(div);
     }
